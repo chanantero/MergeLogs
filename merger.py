be6f9ea6e447196ef3406f49a83b62d65cd830f2
ruben.chulia <ruben.chulia@casa-systems.com>
2019-07-01 13:22:43 +0200

    Fixes

--- merger.py
+++ merger.py
@@ -12,28 +12,40 @@ def merge_logs(log_file_1, log_file_2, merged_log_file_name):
 def merge_logs_lines(log_lines_1, log_lines_2):
 
     date_time_1 = []
+    inds_1 = []
+    count = 0
     for line in log_lines_1:
-        date_time_1.append(line_date_time(line))
+        date_time = line_date_time(line)
+        if date_time is not None:
+            inds_1.append(count)
+            date_time_1.append(date_time)
+        count = count + 1
 
     date_time_2 = []
+    inds_2 = []
+    count = 0
     for line in log_lines_2:
-        date_time_2.append(line_date_time(line))
+        date_time = line_date_time(line)
+        if date_time is not None:
+            inds_2.append(count)
+            date_time_2.append(date_time)
+        count = count + 1
 
     sorted_info = sort_date_time_lists(date_time_1, date_time_2)
 
     merged_log = []
     for i in range(0, sorted_info['num_elements']):
         if sorted_info['groups'][i] == 0:
-            merged_log.append(log_lines_1[sorted_info['indices'][i]])
+            merged_log.append(log_lines_1[inds_1[sorted_info['indices'][i]]])
         elif sorted_info['groups'][i] == 1:
-            merged_log.append(log_lines_2[sorted_info['indices'][i]])
+            merged_log.append(log_lines_2[inds_2[sorted_info['indices'][i]]])
 
     return merged_log
 
 
 def sort_date_time_lists(date_time_1, date_time_2):
     date_times = date_time_1 + date_time_2
-    date_times_original = date_times.copy()
+    date_times_original = list(date_times)
     ind_1 = list(range(0, len(date_time_1)))
     ind_2 = list(range(0, len(date_time_2)))
     inds_original = ind_1 + ind_2
@@ -53,10 +65,13 @@ def sort_date_time_lists(date_time_1, date_time_2):
 
 
 def line_date_time(line):
-    m = re.search(r'\[(.*\d+.*)\]', line)
-    date_time_string = m.group(1)
-    date_time = datetime.datetime.strptime(date_time_string,"%Y%m%d %H:%M:%S:%f")
-    return date_time
+    m = re.search(r'\[([\d|\s|:]+)\]', line)
+    if m is not None:
+        date_time_string = m.group(1)
+        date_time = datetime.datetime.strptime(date_time_string,"%Y%m%d %H:%M:%S:%f")
+        return date_time
+    else:
+        return None 
 
 
 def file_to_line_list(file_name):
--- tests_merger.py
+++ tests_merger.py
@@ -41,27 +41,39 @@ class TestsMerger(unittest.TestCase):
 
     def test_should_merge_log_lines(self):
         # Given
-        log_lines_1 = ['[20190628 11:00:00:000000] blabla', '[20190628 12:00:00:000000] blabla']
-        log_lines_2 = ['[20190628 11:30:00:000000] blabla', '[20190628 12:00:01:000000] blabla']
-        expected_log_lines = ['[20190628 11:00:00:000000] blabla', '[20190628 11:30:00:000000] blabla', '[20190628 12:00:00:000000] blabla', '[20190628 12:00:01:000000] blabla']
+        line1 = '[20190701 07:57:34:108335] [1m[37m[Info]    [0m[0;34m[MME 1]->[eNB] Error Indication (unknown target id)[0m'
+        line2 = '[20190701 07:57:34:108340] [1m[37m[Info]    [0m000f4009000001000240020160'
+        line3 = '[20190701 07:58:38:874164] [1m[37m[Info]    [0mDL[1m[32m A[0m[   0][   0][1m[31m N[0m[   0][   0][1m[33m D[0m[   0][   0]  -  UL[1m[32m A[0m[   0][1m[31m N[0m[   0][1m[33m D[0m[   0] - 0 UEs - %CPU  9.84'
+        line4 = '[20190701 07:58:39:874183] [1m[37m[Info]    [0mDL[1m[32m A[0m[   0][   0][1m[31m N[0m[   0][   0][1m[33m D[0m[   0][   0]  -  UL[1m[32m A[0m[   0][1m[31m N[0m[   0][1m[33m D[0m[   0] - 0 UEs - %CPU  9.72'
+        log_lines_1 = [line1, 'no datetime', line3]
+        log_lines_2 = [line2, line4]
+        expected_log_lines = [line1, line2, line3, line4]
 
         # When
         merged_log = merger.merge_logs_lines(log_lines_1, log_lines_2)
 
         # Then
-        self.assertEqual([1,4,2], [1,4,2])
-        # self.assertEqual(expected_log_lines, merged_log)
+        self.assertEqual(expected_log_lines, merged_log)
 
     def test_get_date_time_from_log_line(self):
         # Given
-        log_line = '[20190628 11:00:00:000000] blabla'
+        log_line = '[20190701 09:42:02:047427] [1m[37m[Info]    [0m[Cell] Synchronized with GPS'
         expected_date_time = datetime.datetime(2019, 6, 28, 11, 0, 0, 0)
 
         # When
         date_time = merger.line_date_time(log_line)
 
         # Then
-        self.assertEqual(expected_date_time, date_time)
+
+    def test_dont_fail_when_there_is_no_date_time(self):
+        # Given
+        log_line = 'blabla'
+
+        # When
+        date_time = merger.line_date_time(log_line)
+
+        # Then
+        self.assertEqual(None, date_time)
 
     def test_text_file_to_line_list(self):
         # Given
